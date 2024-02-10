cddcc8354d30657aef118070f561b55249ac4705
ErshadGS <ershadzh7@gmail.com>
2024-02-09 23:06:45 -0500

    Revert "Completed keyframe animations"
    This reverts commit 0796559c1be01398bcb23879dedfc5292852bd6a.

--- PandaEngine/Animation.h
+++ PandaEngine/Animation.h
@@ -3,7 +3,6 @@
 #include <vector>
 #include <string>
 #include "cMesh.h"
-#include <functional>
 
 enum class EasingType
 {
@@ -13,68 +12,50 @@ enum class EasingType
 	SineEaseInOut
 };
 
-struct KeyFrame
-{
-	KeyFrame(double time, EasingType type = EasingType::Linear)
-		: time(time)
-		, easingType(type)
-	{ }
-	double time;
-	EasingType easingType;
-	std::function<void()> callback;
-};
 
-
-struct PositionKeyFrame : KeyFrame
+struct PositionKeyFrame
 {
 	PositionKeyFrame(const glm::vec3& position, double time, EasingType type = EasingType::Linear)
 		: position(position)
-		, KeyFrame(time, type)
+		, time(time)
+		, easingType(type)
 	{ }
 
+	double time;
 	glm::vec3 position;
+	EasingType easingType;
 };
 
-struct RotationKeyFrame : KeyFrame
+struct RotationKeyFrame
 {
 	RotationKeyFrame(const glm::vec3& rotation, double time, EasingType type = EasingType::Linear)
 		: rotation(rotation)
-		, KeyFrame(time, type)
+		, time(time)
+		, easingType(type)
 	{ }
+	double time;
 	glm::vec3 rotation;
+	EasingType easingType;
 };
 
-struct ScaleKeyFrame : KeyFrame
+struct ScaleKeyFrame
 {
 	ScaleKeyFrame(const glm::vec3& scale, double time, EasingType type = EasingType::Linear)
 		: scale(scale)
-		, KeyFrame(time, type)
+		, time(time)
+		, easingType(type)
 	{ }
+	double time;
 	glm::vec3 scale;
+	EasingType easingType;
 };
 
 struct Animation
 {
-	Animation(std::string name, cMesh* mesh)
-		: name(name)
-		, mesh(mesh)
-	{ }
-
 	std::string name;
 	cMesh* mesh;
 	std::vector<PositionKeyFrame> positionKeyFrames;
 	std::vector<RotationKeyFrame> rotationKeyFrames;
 	std::vector<ScaleKeyFrame> scaleKeyFrames;
 	double time;
-	
-	bool IsDone()
-	{
-		if (time >= positionKeyFrames[positionKeyFrames.size() - 1].time
-			&& time >= rotationKeyFrames[rotationKeyFrames.size() -1].time
-			&& time >= scaleKeyFrames[scaleKeyFrames.size() - 1].time)
-		{
-			return true;
-		}
-		return false;
-	}
 };
--- PandaEngine/AnimationSystem.cpp
+++ PandaEngine/AnimationSystem.cpp
@@ -1,7 +1,5 @@
 #include "AnimationSystem.h"
 #include <glm/gtx/easing.hpp>
-#include <iostream>
-
 AnimationSystem::AnimationSystem()
 {
 }
@@ -12,76 +10,45 @@ AnimationSystem::~AnimationSystem()
 
 void AnimationSystem::Update(float dt)
 {
-	// Update all animations
 	for (Animation* anim : m_animations)
 	{
-		// Skip if paused
 		if (m_isPaused)
 		{
 			continue;
 		}
 
-		UpdateAnimation(anim, dt);
-	}
+		// Update the animation
+		anim->time = dt;
+		double time = anim->time * animationSpeed;
+		//time += dt;
 
-}
-
-void AnimationSystem::AddAnimation(Animation* anim)
-{
-	// Add an animation to the system
-	m_animations.push_back(anim);
-}
-
-
-void AnimationSystem::Pause()
-{
-	// Pause the animation system
-	m_isPaused = true;
-}
-
-void AnimationSystem::Resume()
-{
-	// Resume the animation system
-	m_isPaused = false;
-}
-
-
-
-void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
-{
-	// Update the animation
-	anim->time = dt;
-	double time = anim->time * animationSpeed;
-	cMesh* affectedMesh = anim->mesh;
+		cMesh* affectedMesh = anim->mesh;
 #pragma region POSITION
-
-
-	// Position
-	if (anim->positionKeyFrames.size() == 1)
-	{
-		affectedMesh->drawPosition = anim->positionKeyFrames[0].position;
-	}
-	else
-	{
-		// Find our position keyframes (Start and End)
-		int KeyFrameEndIndex = 0;
-		for (; KeyFrameEndIndex < anim->positionKeyFrames.size(); KeyFrameEndIndex++)
+		// Position
+		if (anim->positionKeyFrames.size() == 1)
 		{
-			if (anim->positionKeyFrames[KeyFrameEndIndex].time > time)
-			{
-				break;
-			}
-		}
-
-		if (KeyFrameEndIndex >= anim->positionKeyFrames.size())
-		{
-			// we are at or past the last key frame use the last keyframe only
-			affectedMesh->drawPosition = anim->positionKeyFrames[KeyFrameEndIndex - 1].position;
-			//return;
+			affectedMesh->drawPosition = anim->positionKeyFrames[0].position;
 		}
 		else
 		{
+			// Find our position keyframes (Start and End)
+			int KeyFrameEndIndex = 0;
+			for (; KeyFrameEndIndex < anim->positionKeyFrames.size(); KeyFrameEndIndex++)
+			{
+				if (anim->positionKeyFrames[KeyFrameEndIndex].time > time)
+				{
+					break;
+				}
+			}
+
+			if (KeyFrameEndIndex >= anim->positionKeyFrames.size())
+			{
+				// we are at or past the last key frame use the last keyframe only
+				affectedMesh->drawPosition = anim->positionKeyFrames[KeyFrameEndIndex - 1].position;
+				return;
+			}
 			int KeyFrameStartIndex = KeyFrameEndIndex - 1;
+
 
 			PositionKeyFrame startKeyFrame = anim->positionKeyFrames[KeyFrameStartIndex];
 			PositionKeyFrame endKeyFrame = anim->positionKeyFrames[KeyFrameEndIndex];
@@ -89,13 +56,10 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 			float percent = (time - startKeyFrame.time) / (endKeyFrame.time - startKeyFrame.time);
 			float result = 0.f;
 
-			// Check if we are at the end of frame
-			if (percent > 0.99f)
+			if (percent >= 0.99f)
 			{
-				if (startKeyFrame.callback)
-				{
-					startKeyFrame.callback();
-				}
+				//percent = 1.0f;
+				bool breakpoint = true;
 			}
 
 			switch (endKeyFrame.easingType)
@@ -120,36 +84,32 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 			glm::vec3 delta = endKeyFrame.position - startKeyFrame.position;
 			affectedMesh->drawPosition = startKeyFrame.position + delta * result;
 		}
-		
-	}
 #pragma endregion
 
 #pragma region ROTATION
-	// Rotation
-	if (anim->rotationKeyFrames.size() == 1)
-	{
-		affectedMesh->setRotationFromEuler(anim->rotationKeyFrames[0].rotation);
-	}
-	else
-	{
-		// Find our position keyframes (Start and End)
-		int KeyFrameEndIndex = 0;
-		for (; KeyFrameEndIndex < anim->rotationKeyFrames.size(); KeyFrameEndIndex++)
+		// Rotation
+		if (anim->rotationKeyFrames.size() == 1)
 		{
-			if (anim->rotationKeyFrames[KeyFrameEndIndex].time > time)
-			{
-				break;
-			}
-		}
-
-		if (KeyFrameEndIndex >= anim->rotationKeyFrames.size())
-		{
-			// we are at or past the last key frame use the last keyframe only
-			affectedMesh->setRotationFromEuler(anim->rotationKeyFrames[KeyFrameEndIndex - 1].rotation);
-			//return;
+			affectedMesh->setRotationFromEuler(anim->rotationKeyFrames[0].rotation);
 		}
 		else
 		{
+			// Find our position keyframes (Start and End)
+			int KeyFrameEndIndex = 0;
+			for (; KeyFrameEndIndex < anim->rotationKeyFrames.size(); KeyFrameEndIndex++)
+			{
+				if (anim->rotationKeyFrames[KeyFrameEndIndex].time > time)
+				{
+					break;
+				}
+			}
+
+			if (KeyFrameEndIndex >= anim->rotationKeyFrames.size())
+			{
+				// we are at or past the last key frame use the last keyframe only
+				affectedMesh->setRotationFromEuler(anim->rotationKeyFrames[KeyFrameEndIndex - 1].rotation);
+				return;
+			}
 			int KeyFrameStartIndex = KeyFrameEndIndex - 1;
 
 			RotationKeyFrame startKeyFrame = anim->rotationKeyFrames[KeyFrameStartIndex];
@@ -158,20 +118,11 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 			float percent = (time - startKeyFrame.time) / (endKeyFrame.time - startKeyFrame.time);
 			float result = 0.f;
 
-			// Check if we are at the end of frame
-			if (percent > 0.99f)
-			{
-				if (startKeyFrame.callback)
-				{
-					startKeyFrame.callback();
-				}
-			}
-
 			switch (endKeyFrame.easingType)
 			{
 				case EasingType::Linear:
 					result = percent;
-					anim->mesh->wholeObjectDebugColourRGBA = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+					anim->mesh->wholeObjectDebugColourRGBA = glm::vec4(1.0f, 1.0f, 1.0f,1.0f);
 					break;
 				case EasingType::SineEaseIn:
 					result = glm::sineEaseIn(percent);
@@ -196,35 +147,31 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 			glm::vec3 euler = glm::eulerAngles(offset);
 			affectedMesh->setRotationFromEuler(euler);
 		}
-		
-	}
-#pragma endregion
+		#pragma endregion
 
 #pragma region SCALE
-	if (anim->scaleKeyFrames.size() == 1)
-	{
-		affectedMesh->setUniformDrawScale(anim->scaleKeyFrames[0].scale.x);
-	}
-	else
-	{
-		// Find our position keyframes (Start and End)
-		int KeyFrameEndIndex = 0;
-		for (; KeyFrameEndIndex < anim->scaleKeyFrames.size(); KeyFrameEndIndex++)
+		if (anim->scaleKeyFrames.size() == 1)
 		{
-			if (anim->scaleKeyFrames[KeyFrameEndIndex].time > time)
-			{
-				break;
-			}
-		}
-
-		if (KeyFrameEndIndex >= anim->scaleKeyFrames.size())
-		{
-			// we are at or past the last key frame use the last keyframe only
-			affectedMesh->setUniformDrawScale(anim->scaleKeyFrames[KeyFrameEndIndex - 1].scale.x);
-			//return;
+			affectedMesh->setUniformDrawScale(anim->scaleKeyFrames[0].scale.x);
 		}
 		else
 		{
+			// Find our position keyframes (Start and End)
+			int KeyFrameEndIndex = 0;
+			for (; KeyFrameEndIndex < anim->scaleKeyFrames.size(); KeyFrameEndIndex++)
+			{
+				if (anim->scaleKeyFrames[KeyFrameEndIndex].time > time)
+				{
+					break;
+				}
+			}
+
+			if (KeyFrameEndIndex >= anim->scaleKeyFrames.size())
+			{
+				// we are at or past the last key frame use the last keyframe only
+				affectedMesh->setUniformDrawScale(anim->scaleKeyFrames[KeyFrameEndIndex - 1].scale.x);
+				return;
+			}
 			int KeyFrameStartIndex = KeyFrameEndIndex - 1;
 
 			ScaleKeyFrame startKeyFrame = anim->scaleKeyFrames[KeyFrameStartIndex];
@@ -232,15 +179,6 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 
 			float percent = (time - startKeyFrame.time) / (endKeyFrame.time - startKeyFrame.time);
 			float result = 0.f;
-
-			// Check if we are at the end of frame
-			if (percent > 0.99f)
-			{
-				if (startKeyFrame.callback)
-				{
-					startKeyFrame.callback();
-				}
-			}
 
 			switch (endKeyFrame.easingType)
 			{
@@ -261,9 +199,21 @@ void AnimationSystem::UpdateAnimation(Animation* anim, float dt)
 			glm::vec3 delta = endKeyFrame.scale - startKeyFrame.scale;
 			affectedMesh->setUniformDrawScale(startKeyFrame.scale.x + delta.x * result);
 		}
-		
+		#pragma endregion
 	}
-#pragma endregion
-
 }
 
+void AnimationSystem::AddAnimation(Animation* anim)
+{
+	m_animations.push_back(anim);
+}
+
+void AnimationSystem::Pause()
+{
+	m_isPaused = true;
+}
+
+void AnimationSystem::Resume()
+{
+	m_isPaused = false;
+}
