92c3acb76ae24c7e6ffda7e3dd974aac3f69444d
ErshadGS <ershadzh7@gmail.com>
2024-04-03 01:43:17 -0400

    physx init

--- PandaEngine/main.cpp
+++ PandaEngine/main.cpp
@@ -3,6 +3,11 @@
 #include "Random.h"
 #include <iostream>
 #include "AnimationSystem.h"
+//#include <px>
+
+#include <PhysX/PxPhysicsAPI.h>
+#include <PhysX/pvd/PxPvd.h>
+
 extern Camera* camera;
 int keyHit = 0;
 
@@ -20,6 +25,83 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     camera->ProcessMouseMovement(xpos, ypos);
+}
+
+using namespace physx;
+PxDefaultAllocator		gAllocator;
+PxDefaultErrorCallback	gErrorCallback;
+
+PxFoundation* gFoundation = NULL;
+PxPhysics* gPhysics = NULL;
+
+PxDefaultCpuDispatcher* gDispatcher = NULL;
+PxScene* gScene = NULL;
+
+PxMaterial* gMaterial = NULL;
+
+PxPvd* gPvd = NULL;
+
+PxReal stackZ = 10.0f;
+
+#define PVD_HOST "127.0.0.1"
+
+void createStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
+{
+    PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
+    for (PxU32 i = 0; i < size; i++)
+    {
+        for (PxU32 j = 0; j < size - i; j++)
+        {
+            PxTransform localTm(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 + 1), 0) * halfExtent);
+            PxRigidDynamic* body = gPhysics->createRigidDynamic(t.transform(localTm));
+            body->attachShape(*shape);
+            PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+            gScene->addActor(*body);
+        }
+    }
+    shape->release();
+}
+
+void initPhysics(bool interactive)
+{
+    gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+    gPvd = PxCreatePvd(*gFoundation);
+    PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+    gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+    gDispatcher = PxDefaultCpuDispatcherCreate(2);
+    sceneDesc.cpuDispatcher = gDispatcher;
+    sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+    gScene = gPhysics->createScene(sceneDesc);
+
+    PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    if (pvdClient)
+    {
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+    }
+    gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+    PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+    gScene->addActor(*groundPlane);
+
+    for (PxU32 i = 0; i < 5; i++)
+        createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);
+
+   /* if (!interactive)
+        createDynamic(PxTransform(PxVec3(0, 40, 100)), PxSphereGeometry(10), PxVec3(0, -50, -100));*/
+}
+
+void stepPhysics(/*bool interactive*/)
+{
+    gScene->simulate(1.0f / 60.0f);
+    gScene->fetchResults(true);
 }
 
 int main(void)
@@ -46,9 +128,13 @@ int main(void)
     float currTime = 0;
     float myTime = 0;
 
+    initPhysics(true);
+
+
     while (!glfwWindowShouldClose(engine.window))
     {
-        engine.Update();  
+        engine.Update();
+        stepPhysics();
     }
 
     engine.ShutDown();
